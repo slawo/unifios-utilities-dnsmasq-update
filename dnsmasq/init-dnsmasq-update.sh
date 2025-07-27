3dbb6bab880f4978d4c73a265d09f0a3605b3ff7
Slawomir Caluch <scaluch@gmail.com>
2025-07-27 13:35:10 +0100

    feat: add a script to initialize the update script and cron job

---
+++ dnsmasq/init-dnsmasq-update.sh
@@ -0,0 +1,26 @@
+#! /bin/bashDATA_DIR="/data"
+case "$(ubnt-device-info firmware || true)" in
+1*)
+	DATA_DIR="/mnt/data"
+	;;
+2* | 3* | 4*)
+	DATA_DIR="/data"
+	;;
+*)
+	echo "ERROR: No persistent storage found." 1>&2
+	exit 1
+	;;
+esac
+
+# Check if there is a DNSMASQ cronjob file in /etc/cron.d/dnsmasq
+if [ ! -f /etc/cron.d/dnsmasq ]; then
+	echo "Creating the DNSMASQ cronjob file."
+	cat <<EOF > /etc/cron.d/dnsmasq
+# DNSMASQ cronjob to update DNS records
+# Run every minute
+* * * * * root $DATA_DIR/dnsmasq/update-dns.sh >/dev/null 2>&1
+EOF
+fi
+
+# Run the scrip for the first time
+"$DATA_DIR/dnsmasq/update-dns.sh"
